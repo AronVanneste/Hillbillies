0d662f013d61ce37c674109adb5333d5a978b6d1
AronVanneste <vanneste.aron@gmail.com>
2016-05-13 23:10:55 +0200

    Update AttackStatement.java

--- AttackStatement.java
+++ AttackStatement.java
@@ -7,7 +7,11 @@ public class AttackStatement extends UnitStatement {
 	public AttackStatement(UnitExpression unit, SourceLocation source) {
 		super(unit, source);
 	}
-
+	/**
+	 * Executes the attackstatement, lets the unit attack an other unit
+	 * 
+	 * @post The unit of the AttacksStatement is attacking the given unit
+	 */ 
 	@Override
 	public void execute() {
 		if (this.isAssigned())
