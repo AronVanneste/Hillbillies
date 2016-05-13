8f90aec1d0a3df3173e561565b6f167b14890965
AronVanneste <vanneste.aron@gmail.com>
2016-05-13 23:18:36 +0200

    Update CheckAliveExpression.java

--- CheckAliveExpression.java
+++ CheckAliveExpression.java
@@ -7,7 +7,11 @@ public class CheckAliveExpression extends StatusExpression {
 	public CheckAliveExpression(UnitExpression unit, SourceLocation source) {
 		super(unit, source);
 	}
-
+	/**
+	 * Checks whether the unit assigned to the expression is terminated
+	 * 
+	 * @return Returns whether or not the unit is terminated
+	 */ 
 	@Override
 	public Boolean evaluate() {
 		return !this.getPassiveUnit().evaluate().isTerminated();
