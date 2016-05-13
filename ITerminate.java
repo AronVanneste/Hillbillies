bb77ae53da78721a9a136922397b2f1eb86c8bab
AronVanneste <vanneste.aron@gmail.com>
2016-05-13 23:42:30 +0200

    Update ITerminate.java

--- ITerminate.java
+++ ITerminate.java
@@ -1,7 +1,11 @@
 package hillbillies.model;
 
 public interface ITerminate {
-	
+	/**
+	 * An interface that gives Unit, World, Faction and Raw...
+	 */
+	 
+	 
 	public abstract void terminate();
 	
 	public abstract boolean isTerminated();
