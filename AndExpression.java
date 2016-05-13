8a4cfe4db0d3381913ea2dc4e3578ce8d5d320af
AronVanneste <vanneste.aron@gmail.com>
2016-05-13 22:49:12 +0200

    Update AndExpression.java

--- AndExpression.java
+++ AndExpression.java
@@ -7,7 +7,14 @@ public class AndExpression extends BinaryExpression {
 	public AndExpression(BooleanExpression left, BooleanExpression right, SourceLocation source) {
 		super(left, right, source);
 	}
-
+	
+	/**
+	 * Evaluates the AndExpression
+	 * 
+	 * @return Returns true if and only if both expressions are true
+	 *
+	 */
+	  
 	@Override
 	public Boolean evaluate() {
 		return (this.getRightExpression().evaluate()
