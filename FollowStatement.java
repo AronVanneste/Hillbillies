0f04cac5ecc7808945ad919a53774158c765b9bc
AronVanneste <vanneste.aron@gmail.com>
2016-05-13 23:37:11 +0200

    Update FollowStatement.java

--- FollowStatement.java
+++ FollowStatement.java
@@ -7,7 +7,11 @@ public class FollowStatement extends UnitStatement {
 	public FollowStatement(UnitExpression unit, SourceLocation source) {
 		super(unit, source);
 	}
-
+	/**
+	 * Executes the followStatement
+	 * 
+	 * @post The unit assigned to the expression follows the given unit
+	 */
 	@Override
 	public void execute() {
 		if (this.isAssigned())
