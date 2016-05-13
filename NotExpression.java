3af92ee9c27c4931820445e96d93512946a0a596
AronVanneste <vanneste.aron@gmail.com>
2016-05-13 23:53:09 +0200

    Update NotExpression.java

--- NotExpression.java
+++ NotExpression.java
@@ -7,7 +7,11 @@ public class NotExpression extends UnaryExpression {
 	public NotExpression(BooleanExpression expression, SourceLocation source) {
 		super(expression, source);
 	}
-
+	/**
+	 * Evaluates the notExpression
+	 * 
+	 * @return Returs the opposite of the expression
+	 */
 	@Override
 	public Boolean evaluate() {
 		return (!this.getExpression().evaluate());
