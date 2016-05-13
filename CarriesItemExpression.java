b3e33b301400b4ef46f09b8bd37d30148006a268
AronVanneste <vanneste.aron@gmail.com>
2016-05-13 23:17:12 +0200

    Update CarriesItemExpression.java

--- CarriesItemExpression.java
+++ CarriesItemExpression.java
@@ -7,7 +7,11 @@ public class CarriesItemExpression extends StatusExpression {
 	public CarriesItemExpression(UnitExpression unit, SourceLocation source) {
 		super(unit, source);
 	}
-
+	/**
+	 * Evaluates the expression
+	 * 
+	 * @return Returns whether or not the unit assigned to the expression if carrying a boulder or log
+	 */ 
 	@Override
 	public Boolean evaluate() {
 		return (this.getPassiveUnit().evaluate().isCarryingBoulder() | 
