f93de79c424a4d6314a78cb1060829015b3cb4ab
AronVanneste <vanneste.aron@gmail.com>
2016-05-13 23:36:04 +0200

    Update FalseExpression.java

--- FalseExpression.java
+++ FalseExpression.java
@@ -7,7 +7,11 @@ public class FalseExpression extends BooleanExpression {
 	public FalseExpression(SourceLocation sourceLocation) {
 		super(sourceLocation);
 	}
-
+	/**
+	 * Evaluates the falseExpression
+	 * 
+	 * @return Returns false
+	 */
 	@Override
 	public Boolean evaluate() {
 		return false;
