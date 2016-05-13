130583bf6c2b48cd1c742a4bcb884edc855182be
AronVanneste <vanneste.aron@gmail.com>
2016-05-13 23:25:04 +0200

    Update CheckSolidExpression.java

--- CheckSolidExpression.java
+++ CheckSolidExpression.java
@@ -8,7 +8,15 @@ public class CheckSolidExpression extends TerrainExpression {
 	public CheckSolidExpression(PositionExpression pos, SourceLocation sourceLocation) {
 		super(pos, sourceLocation);
 	}
-
+	/**
+	 * Evaluates the CheckSolidExpression
+	 * 
+	 * @return Returns whether or not the position assigned to the expression is solid
+	 * 
+	 * @throws Throws IllegalArgumentException if the expression is not assigned to a unit
+	 * 
+	 * @throws Throws IllegalUnitException if the unit does not belong to a world
+	*/
 	@Override
 	public Boolean evaluate() throws IllegalArgumentException, IllegalUnitException {
 		if (this.isAssigned()) {
