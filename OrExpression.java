196e8d7f94a30c4d379ce66967cdd699e9bb17f3
AronVanneste <vanneste.aron@gmail.com>
2016-05-13 23:54:13 +0200

    Update OrExpression.java

--- OrExpression.java
+++ OrExpression.java
@@ -7,7 +7,11 @@ public class OrExpression extends BinaryExpression {
 	public OrExpression(BooleanExpression left, BooleanExpression right, SourceLocation source) {
 		super(left, right, source);
 	}
-
+	/**
+	 * Evaluates the OrExpression
+	 * 
+	 * @return Returns whether or not the left or right expression evaluate to true
+	 */
 	@Override
 	public Boolean evaluate() {
 		return (this.getRightExpression().evaluate() || 
